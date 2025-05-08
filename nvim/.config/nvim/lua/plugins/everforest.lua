3f8e059b2abd47b194b3bcdf5bccc45a19848502
maxBe <maxime.begaud@icloud.com>
2025-05-09 04:30:24 +0900

    fix mason

--- nvim/.config/nvim/lua/plugins/everforest.lua
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -6,7 +6,6 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("everforest").setup({
-        -- This is probably the main issue - transparency level might be too low
         transparent_background_level = 2,
       })
     end,
