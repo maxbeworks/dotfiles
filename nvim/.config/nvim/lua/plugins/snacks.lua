1424b1608570d96b9a2f3ca2cddc28594505f25d
maxBe <maxime.begaud@icloud.com>
2025-05-09 16:31:45 +0900

    still sucks, just less

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,20 +1,49 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    dashboard = {
+      preset = {
+        header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
+    },
     picker = {
-          hidden=true,
-          ignored=true,
+      hidden = true,
+      ignored = true,
       sources = {
         files = {
-          hidden=true,
-          ignored=true,
+          hidden = true,
+          ignored = true,
         },
         explorer = {
           auto_close = true,
-          layout = {
-            preset = "default",
-            preview = true,
-          },
+          -- layout = {
+          --   preset = "default",
+          --   preview = true,
+          -- },
         },
       },
     },
