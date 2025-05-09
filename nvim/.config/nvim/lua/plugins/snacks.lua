6a7a1b8df9d2c9d19d69ee2a99bdb7e2eea9f148
maxBe <maxime.begaud@icloud.com>
2025-05-09 10:59:51 +0900

    problem with snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,19 +2,13 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
-          hidden=true,
-          ignored=true,
       sources = {
-        files = {
-          hidden=true,
-          ignored=true,
-        },
         explorer = {
           auto_close = true,
-          layout = {
-            preset = "default",
-            preview = true,
-          },
+          -- layout = {
+          --   preset = "default",
+          --   preview = true,
+          -- },
         },
       },
     },
