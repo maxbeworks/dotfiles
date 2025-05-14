e4f4d2916775b61d1b56993a698b72df8fd45f6f
maxBe <maxime.begaud@icloud.com>
2025-05-14 17:48:30 +0900

    no one reading these commits but me

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
+  -- config = function()
+  --   local lspconfig = require("lspconfig")
+  --   lspconfig.sourcekit.setup({})
+  -- end,
   opts = {
     autoformat = false,
+    -- servers = {
+    --   sourcekit = {
+    --     cmd = { "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp" },
+    --   },
+    -- },
   },
 }
