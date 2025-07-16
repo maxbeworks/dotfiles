b284eb62561fd80458694df165a8f521973319d5
maxBe <maxime.begaud@icloud.com>
2025-07-16 19:10:06 +0900

    avante not my thing

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
+  { "saghen/blink.cmp", enabled = false },
 }
